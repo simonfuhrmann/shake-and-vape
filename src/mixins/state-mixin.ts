f39c11ee56af73b59684db6ee68643afb94340c2
Simon Fuhrmann <simonfuhrmann@gmx.de>
2021-03-29 21:31:50 -0700

    Add global state mixin helper

---
+++ src/mixins/state-mixin.ts
@@ -0,0 +1,34 @@
+import {LitElement} from 'lit-element';
+
+import {State, stateManager} from '../modules/state-types';
+
+type Constructor<T = LitElement> = new (...args: any[]) => T;
+
+// Helper mixin for the global state management to reduce boilerplate code.
+export function StateMixin<TBase extends Constructor>(Base: TBase) {
+  return class extends Base {
+    private readonly mixinStateListener = this.stateChanged.bind(this);
+
+    constructor(...args: any[]) {
+      super(...args);
+    }
+
+    connectedCallback() {
+      super.connectedCallback();
+      stateManager.addListener(this.mixinStateListener);
+    }
+
+    disconnectedCallback() {
+      super.disconnectedCallback();
+      stateManager.removeListener(this.mixinStateListener);
+    }
+
+    getState() {
+      return stateManager.getState();
+    }
+
+    stateChanged(_newState: State, _oldState: State|null) {
+      // No-op. Will be called on sub-classes.
+    }
+  };
+}
