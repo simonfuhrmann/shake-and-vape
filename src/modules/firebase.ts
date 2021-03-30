96840cb9c3c28f7400bc14d5f569b360cbcaa73f
Simon Fuhrmann <simonfuhrmann@gmx.de>
2021-03-29 21:32:51 -0700

    Add Firebase config and API

---
+++ src/modules/firebase.ts
@@ -0,0 +1,23 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+import {firebaseConfig} from '../config/firebase';
+
+class Firebase {
+  private app: firebase.app.App;
+
+  constructor() {
+    this.app = firebase.initializeApp(firebaseConfig);
+  }
+
+  getAuth(): firebase.auth.Auth {
+    return this.app.auth();
+  }
+
+  getFirestore(): firebase.firestore.Firestore {
+    return this.app.firestore();
+  }
+}
+
+export const firebaseApi = new Firebase();
