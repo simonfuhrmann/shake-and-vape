f9a270644e59b19f071f5735e67ded8ef99958af
Simon Fuhrmann <simonfuhrmann@gmx.de>
2021-04-04 22:34:31 -0700

    Add user details to global state

--- src/modules/state-actions.ts
+++ src/modules/state-actions.ts
@@ -1,12 +1,20 @@
 import firebase from 'firebase/app';
 
-import {State, stateManager, getInitialState} from './state-types';
+import {State, UserDetails, stateManager, getInitialState} from './state-types';
 
 // Sets the current Firebase user.
 export function setCurrentUser(firebaseUser: firebase.User|null) {
   const action = (state: State) => {
     if (state.firebaseUser === firebaseUser) return state;
     return {...state, firebaseUser};
+  };
+  stateManager.processAction(action);
+}
+
+export function setUserDetails(userDetails: UserDetails|null) {
+  const action = (state: State) => {
+    if (state.userDetails === userDetails) return state;
+    return {...state, userDetails};
   };
   stateManager.processAction(action);
 }
--- src/modules/state-types.ts
+++ src/modules/state-types.ts
@@ -2,13 +2,19 @@ import firebase from 'firebase/app';
 
 import {StateManager} from './state-manager';
 
+export interface UserDetails {
+  name: string;
+}
+
 export interface State {
   firebaseUser: firebase.User|null;
+  userDetails: UserDetails|null;
 }
 
 export function getInitialState(): State {
   return {
     firebaseUser: null,
+    userDetails: null,
   };
 }
 
